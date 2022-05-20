46d3deebe81810e838e8c349385d29669d6e3937
orlogue <102894674+orlogue@users.noreply.github.com>
2022-05-20 17:34:47 +1000

    Update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,38 @@
 <template>
   <div id="app">
-    cxvm,xfkmfxdkflxfrrfrfrfrf
+    <HelloWorld/>
+    {{ profiles }}
+<!--    sjglskdjglksdjgs-->
+<!--    <ul>-->
+<!--      <li v-for="(profile, index) in object.data['profiles']" :key="index">{{ profile }}</li>-->
+<!--    </ul>-->
+<!--    <li v-for="(profile, index) in object.data['profiles']" :key="index">{{ profile }}</li>-->
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import HelloWorld from "./components/HelloWorld";
 
+export default {
+  name: 'app',
+  components: {HelloWorld},
+  // data: () => ({
+  //   profiles: []
+  // }),
+  data() {
+    return {
+      profiles: []
+    }
+  },
+  mounted () {
+    axios.get('http://localhost:8000/api/profiles/').then((response) => {
+      this.profiles = response.data
+    })
+  }
+}
 </script>
+
 
 <style>
 #app {
