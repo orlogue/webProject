5d5ccf0284c037220aeb681595fe2284cb4b8892
orlogue <102894674+orlogue@users.noreply.github.com>
2022-07-12 00:34:23 +1000

    Update

--- backend/profiles/admin.py
+++ backend/profiles/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Profile, Building
+from .models import Profile, Building, Telegram
 
 
 class ProfileAdmin(UserAdmin):
     model = Profile
     list_display = ('id', 'phone_number', 'name', 'building', 'room', 'is_staff', 'is_active',)
     list_filter = ('phone_number', 'building', 'is_staff', 'is_active',)
-    fieldsets = (
+    add_fieldsets = (
         (None, {'fields': ('phone_number', 'password', 'building', 'room')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
@@ -20,5 +20,11 @@ class BuildingAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
 
 
+class TelegramAdmin(admin.ModelAdmin):
+    model = Telegram
+    list_display = ['id', 'user', 'telegram_id']
+
+
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Building, BuildingAdmin)
+admin.site.register(Telegram, TelegramAdmin)
